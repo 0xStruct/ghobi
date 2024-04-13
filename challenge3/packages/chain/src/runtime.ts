2d62f71370f137eec38326a3b65c96d52063b0cd
0xstruct <0xstruct@gmail.com>
2024-04-13 20:18:41 +0630

    challenge3 working with spy_manager.test.ts and README

--- challenge3/packages/chain/src/runtime.ts
+++ challenge3/packages/chain/src/runtime.ts
@@ -1,15 +1,19 @@
 import { Balance } from "@proto-kit/library";
 import { Balances } from "./balances";
+import { SpyManager } from "./spy_manager";
 import { ModulesConfig } from "@proto-kit/common";
 
 export const modules = {
   Balances,
+  SpyManager,
 };
 
 export const config: ModulesConfig<typeof modules> = {
   Balances: {
     totalSupply: Balance.from(10_000),
   },
+
+  SpyManager: {},
 };
 
 export default {
